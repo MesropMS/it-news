722c675334a4d49972072bde94cf0282a52ac17b
MesropMS <64807940+MesropMS@users.noreply.github.com>
2020-05-24 20:22:44 +0400

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Post,Group
-from django.core.mail import send_mail
-from django.conf import settings
+import smtplib
 
 
 def main(request):
@@ -29,7 +28,18 @@ def about(request):
 def contact(request):
 	group = Group.objects.all()
 	if request.method == 'POST':
-		send_mail(request.POST.get('sub'), request.POST.get('msg'), 'mesrop.araqelyan.09@gmail.com', [settings.EMAIL_HOST_USER],fail_silently=False)
+		password = 'mospos321'
+		username = 'mesrop.araqelyan.04@gmail.com'
+		subject = request.POST.get('sub')
+		message = request.POST.get('msg')
+		with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+			smtp.ehlo()
+			smtp.starttls()
+			smtp.ehlo()
+			msg = f'Subject:{subject}\n\n{message}'
+	
+			smtp.login(username,password)
+			smtp.sendmail(username,'mesrop.araqelyan.09@gmail.com',msg)
 	return render(request,'contact.html',{'group':group})
 
 def search(request):
