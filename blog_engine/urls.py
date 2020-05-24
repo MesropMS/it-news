18d6db8efbd90be75038bbfd1b29aafcb5574f8c
MesropMS <64807940+MesropMS@users.noreply.github.com>
2020-05-24 13:26:47 +0400

    Add files via upload

---
+++ blog_engine/urls.py
@@ -0,0 +1,29 @@
+"""blog_engine URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib.staticfiles.urls import static
+from django.conf import settings
+from blog.views import main,group_detail,post_detail,about,contact
+urlpatterns = [
+    path('',main,name='home'),
+    path('about/',about,name='about'),
+	path('contact/',contact,name='contact'),
+	path('group/<str:slug>',group_detail,name='detail'),
+    path('post/<str:post_slug>',post_detail,name='post_detail'),
+    path('admin/', admin.site.urls),
+]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
