27f323164e407041344ce100613a9e98e205f940
MesropMS <64807940+MesropMS@users.noreply.github.com>
2020-05-24 19:26:29 +0400

    Update settings.py

--- blog_engine/settings.py
+++ blog_engine/settings.py
@@ -77,7 +77,7 @@ django_heroku.settings(locals())
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
